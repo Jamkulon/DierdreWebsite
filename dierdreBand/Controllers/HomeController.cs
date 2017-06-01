a88d5e9da395d3dcca0d8890fc3da01bc2e64795
James Kulon <james.kulon@gmail.com>
2017-06-01 07:20:24 -0700

    initial commit

---
+++ dierdreBand/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dierdreBand.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Shows()
+        {
+            ViewData["Message"] = "Your Shows Page.";
+
+            return View();
+        }
+
+        public IActionResult Store()
+        {
+            ViewData["Message"] = "Your StorePage.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
